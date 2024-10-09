b761793096d11ab579dac9d62f24511a44a40808
Your Name <you@example.com>
2024-10-09 09:20:38 +0200

    Ana

---
+++ pinchos.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+const SPEED = 60
+
+var direction = 1
+
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var animated_sprite = $AnimatedSprite2D
